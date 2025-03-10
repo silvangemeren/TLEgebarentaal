f16185cad91d9cbc09d11eeb035b5ae3228626b8
Irpear <145675108+Irpear@users.noreply.github.com>
2025-03-10 15:43:37 +0100

    handvormdata2

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import signs from "./routes/signs.js"
 import videos from "./routes/videos.js"
 import gifs from "./routes/gifs.js"
+import handshapes from "./routes/handshapes.js";
+import mouthshapes from "./routes/mouthshapes.js";
 
 const app = express()
 const port = process.env.EXPRESS_PORT
@@ -21,6 +23,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/videos', videos)
 app.use('/gifs', gifs)
+app.use('/handshapes', handshapes)
+app.use('/mouthshapes', mouthshapes)
 
 app.use((req, res, next) => {
     const acceptHeader = req.headers.accept;
